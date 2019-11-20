940e3ffc11c00b839e90f667b4a68114eff523d1
AbrahamDN <abe.ndinga@gmail.com>
2019-11-20 12:16:33 +0000

    Updates

---
+++ precache-manifest.49fdec7a92dd6d95bb32559fb6d11216.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183a325d959d878ca4e908b65dfc7ae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "383798ed687f20468e0c",
+    "url": "/static/css/main.c74896a8.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "383798ed687f20468e0c",
+    "url": "/static/js/main.c7417929.chunk.js"
+  },
+  {
+    "revision": "d6023e9a5195b54d38ce",
+    "url": "/static/js/runtime-main.e5eb06e0.js"
+  }
+]);
